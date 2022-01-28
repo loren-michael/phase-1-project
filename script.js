ade2eca1af50bd81e2ae8cb695b33f63ad9dd8ea
loren <lorenmichaelh@gmail.com>
2022-01-28 16:50:06 -0600

    add comment button toggles with comment form

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const recDispSec = document.getElementById("recipe-display");
 // DOMContentLoaded
 document.addEventListener("DOMContentLoaded", () => {
     fetchRecipes()
+    btnAddRec.addEventListener("click", addRecipeRequest)
 })// End of DOMContentLoaded
 
 
@@ -26,9 +27,29 @@ function getRecipeDetails(e) {
     .then(renderDetails)
 }
 
+function postComment(e) {
+    const newComment = e.target.value;
+    console.log(newComment)
+    fetch(baseURL + `/${e.target.id}`)
+    .then(resp => resp.json())
+    .then(recObj => {
+        const comments = recObj.comments;
+        comments.push(newComment)
+    })
+    fetch(baseURL + `/${e.target.id}`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({comments})
+    }) .then(renderComment(newComment))
+}
+
 // Rendering
 
 function renderAllRecipes(recArr) {
+    // This hides the recipe display if one was being displayed
     recDispSec.innerHTML = "";
     recArr.forEach(renderOneRecipe)
 }
@@ -41,22 +62,20 @@ function renderOneRecipe(recipe) {
     recListSec.appendChild(recList)
 }
 
+
 function renderDetails(recipe) {
     // Hide the recipe index list when rendering details of a recipe
     recListSec.style="display: none"
-    
     // Create the recipe card
     const recCard = document.createElement("div");
     recCard.className = "card";
     recCard.id = recipe.id;
-    
     // Rendering the recipe image, name and author
     recCard.innerHTML = `
         <img class="center" src="${recipe.img}">
         <h2 align="middle">${recipe.name}</h2>
         <h3 align="middle">By ${recipe.author}</h3>
     `;
-    
     //Creating ingredient header and list using a forEach
     const ingredientHeader = document.createElement("h3");
     ingredientHeader.innerText = "Ingredients:";
@@ -68,7 +87,6 @@ function renderDetails(recipe) {
         ingLi.innerText = `${ing}`;
         recIngList.append(ingLi)
     })
-    
     //Creating instruction header and list using a forEach
     const instrHeader = document.createElement("h3");
     instrHeader.innerText = "Instructions:"
@@ -80,34 +98,41 @@ function renderDetails(recipe) {
         instrLi.innerText = `${instr}`
         recInstructions.append(instrLi)
     })
-    
     // Create a comment section and button
     const commentHeader = document.createElement("h3");
     commentHeader.innerText = "Comments:";
-    const btnAddComment = document.createElement("button")
+    const btnAddComment = document.createElement("button");
+    btnAddComment.id = "btn-add-comment";
     btnAddComment.innerText = " Add a comment "
-    btnAddComment.addEventListener("click", addCommentForm)
+    btnAddComment.addEventListener("click", addCommentForm);
     const commentSection = document.createElement("ul");
     commentSection.className = "card";
-    commentSection.id = "commentSection";
+    commentSection.id = "comment-section";
     let commArr = recipe.comments;
     commArr.forEach(comment => {
         const commentPara = document.createElement("li")
         commentPara.innerText = `${comment}`
         commentSection.append(commentPara)
-    })
-    
+    });
     // Create the back button and add event listener
     const backBtn = document.createElement("button");
     backBtn.innerText = " Back to Index ";
     backBtn.addEventListener("click", revealList);
-    
     recCard.append(ingredientHeader, recIngList, instrHeader, recInstructions, commentHeader, btnAddComment, commentSection, backBtn);
     recDispSec.appendChild(recCard);
 }
 
 
-// Event Listeners
+function renderComment(e){
+    const btnAddComment = document.getElementById("btn-add-comment");
+    btnAddComment.style="display: in-block";
+    const commentSection = document.getElementById("comment-section");
+    const newComment = document.createElement("li");
+    newComment.innerText = e.target.value;
+    commentSection.append(newComment);
+
+}
+
 
 
 
@@ -119,5 +144,23 @@ function revealList() {
 }
 
 function addCommentForm() {
-    console.log("hi")
+    const commentSection = document.getElementById("comment-section");
+    const btnAddComment = document.getElementById("btn-add-comment");
+    btnAddComment.style="display: none";
+    
+    const commentForm = document.createElement("form");
+    commentForm.id = "comment-form";
+    commentForm.innerHTML = `
+        <textarea required placeholder="Add your comment for this recipe"></textarea>
+    `
+    const btnCommSubmit = document.createElement("button")
+    btnCommSubmit.innerText = " Submit Comment ";
+    btnCommSubmit.addEventListener("click", renderComment)
+    commentSection.prepend(commentForm, btnCommSubmit)
+    
+}
+
+function addRecipeRequest() {
+    recDispSec.innerHTML = "";
+    
 }
